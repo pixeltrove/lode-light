d1a0dd16ef871a812fbfecc573180a35a6ea5582
Luchian <luchiancosma@gmail.com>
2021-05-29 10:18:54 +0300

    Animate notification removal

--- source/components/notification.js
+++ source/components/notification.js
@@ -3,12 +3,25 @@
 
 const SELECTOR_NOTIFICATION = ".notification";
 const SELECTOR_BUTTON_DISMISS = ".notification-button-dismiss";
+const CLASS_TRANSITIONING_OUT = "is-transitioning-out";
 
 function Notification(notification) {
   function handleDismiss(event) {
     if (event.target.closest(SELECTOR_BUTTON_DISMISS)) {
-      notification.remove();
+      transitionToHidden();
     }
+  }
+
+  function transitionToHidden() {
+    notification.classList.add(CLASS_TRANSITIONING_OUT);
+
+    notification.addEventListener(
+      "animationend",
+      () => {
+        notification.remove();
+      },
+      { once: true }
+    );
   }
 
   notification.addEventListener("click", handleDismiss);
