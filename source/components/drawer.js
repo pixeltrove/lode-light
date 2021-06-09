55ff2d9dd672ede0f1c1d7ebf966dc7985cd1235
Luchian <luchiancosma@gmail.com>
2021-06-09 16:48:08 +0300

    Add drawer component
    Use drawers as supplementary content holders that appear from one of the viewport edges.

---
+++ source/components/drawer.js
@@ -0,0 +1,124 @@
+// DRAWER
+// -----------------------------------------------------------------------------
+
+const SELECTOR_DRAWER = ".drawer";
+const SELECTOR_BACKDROP = ".drawer-backdrop";
+const CLASS_NO_SCROLL = "no-scroll";
+const CLASS_SHOWN = "is-shown";
+const CLASS_TRANSITIONING_IN = "is-transitioning-in";
+const CLASS_TRANSITIONING_OUT = "is-transitioning-out";
+const DATA_HIDE = "data-hide";
+const DATA_TARGET = "data-target";
+
+function Drawer(drawer) {
+  const drawerId = drawer.id;
+  const trigger = document.querySelector(`[${DATA_TARGET}="${drawerId}"]`);
+  const backdrop = drawer.closest(SELECTOR_BACKDROP);
+
+  function show() {
+    transitionToShown();
+    drawer.setAttribute("tabindex", -1);
+    drawer.focus();
+    toggleScroll();
+
+    drawer.addEventListener("keydown", handleFocusTrap);
+    drawer.addEventListener("click", handleHideClick);
+    backdrop.addEventListener("click", handleBackdropClick);
+    document.addEventListener("keydown", handleEscape);
+  }
+
+  function transitionToShown() {
+    backdrop.classList.add(CLASS_SHOWN);
+    backdrop.classList.add(CLASS_TRANSITIONING_IN);
+    drawer.classList.add(CLASS_TRANSITIONING_IN);
+
+    backdrop.addEventListener(
+      "animationend",
+      () => {
+        backdrop.classList.remove(CLASS_TRANSITIONING_IN);
+        drawer.classList.remove(CLASS_TRANSITIONING_IN);
+      },
+      { once: true }
+    );
+  }
+
+  function hide() {
+    transitionToHidden();
+    toggleScroll();
+
+    drawer.removeEventListener("keydown", handleFocusTrap);
+    drawer.removeEventListener("click", handleHideClick);
+    backdrop.removeEventListener("click", handleBackdropClick);
+    document.removeEventListener("keydown", handleEscape);
+  }
+
+  function transitionToHidden() {
+    backdrop.classList.add(CLASS_TRANSITIONING_OUT);
+    drawer.classList.add(CLASS_TRANSITIONING_OUT);
+
+    backdrop.addEventListener(
+      "animationend",
+      () => {
+        backdrop.classList.remove(CLASS_SHOWN);
+        backdrop.classList.remove(CLASS_TRANSITIONING_OUT);
+        drawer.classList.remove(CLASS_TRANSITIONING_OUT);
+      },
+      { once: true }
+    );
+  }
+
+  function toggleScroll() {
+    if (window.innerHeight >= document.body.scrollHeight) return;
+
+    const scrollPosition = Math.abs(parseInt(document.body.style.top)) || window.scrollY;
+
+    if (document.body.classList.contains(CLASS_NO_SCROLL)) {
+      document.body.classList.remove(CLASS_NO_SCROLL);
+      document.body.style.top = "";
+      window.scroll(0, scrollPosition);
+    } else {
+      document.body.classList.add(CLASS_NO_SCROLL);
+      document.body.style.top = -scrollPosition + "px";
+    }
+  }
+
+  function handleHideClick(event) {
+    if (event.target.hasAttribute(DATA_HIDE)) {
+      hide();
+    }
+  }
+
+  function handleBackdropClick(event) {
+    if (event.target.matches(SELECTOR_BACKDROP)) {
+      hide();
+    }
+  }
+
+  function handleEscape(event) {
+    if (event.key === "Escape") {
+      hide();
+    }
+  }
+
+  function handleFocusTrap(event) {
+    if (event.key === "Tab") {
+      const focusableElements = Array.from(drawer.querySelectorAll("a[href], audio[controls], button:not([disabled]), details, input:not([disabled]), select:not([disabled]), textarea:not([disabled]), video[controls], [contenteditable]"));
+      const lastIndex = focusableElements.length - 1;
+      const focusIndex = focusableElements.indexOf(document.activeElement);
+
+      if ((event.shiftKey && focusIndex === 0) || (event.shiftKey && document.activeElement === drawer)) {
+        event.preventDefault();
+        focusableElements[focusableElements.length - 1].focus();
+      } else if (!event.shiftKey && focusIndex === lastIndex) {
+        event.preventDefault();
+        focusableElements[0].focus();
+      }
+    }
+  }
+
+  trigger.addEventListener("click", show);
+}
+
+const drawers = Array.from(document.querySelectorAll(SELECTOR_DRAWER));
+
+drawers.forEach((drawer) => Drawer(drawer));
