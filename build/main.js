3611f9300ac26a00b24c7dd1be88c2b0529bfc45
Luchian <luchiancosma@gmail.com>
2021-06-22 10:30:08 +0300

    Reorder statements
    Consider case where scroll is not locked first as returning to normal scroll behavior will always occur after this stage.

--- build/main.js
+++ build/main.js
@@ -86,14 +86,14 @@ var CLASS_NO_SCROLL = "no-scroll";
 function toggleScroll() {
   if (window.innerHeight >= document.body.scrollHeight)
     return;
-  const scrollPosition = Math.abs(parseInt(document.body.style.top)) || window.scrollY;
-  if (document.body.classList.contains(CLASS_NO_SCROLL)) {
+  const scrollPosition = window.scrollY || Math.abs(parseInt(document.body.style.top));
+  if (!document.body.classList.contains(CLASS_NO_SCROLL)) {
+    document.body.classList.add(CLASS_NO_SCROLL);
+    document.body.style.top = -scrollPosition + "px";
+  } else {
     document.body.classList.remove(CLASS_NO_SCROLL);
     document.body.style.top = "";
     window.scroll(0, scrollPosition);
-  } else {
-    document.body.classList.add(CLASS_NO_SCROLL);
-    document.body.style.top = -scrollPosition + "px";
   }
 }
 var toggle_scroll_default = toggleScroll;
