e6bcf041b68643613f74115fe0aefe7bd50bdb93
Luchian <luchiancosma@gmail.com>
2021-09-22 16:54:00 +0300

    Remove drawer component
    Drawers are specialized components that should be styled and scripted in accordance with the specific needs of each project.

--- source/main.js
+++ source/main.js
@@ -3,7 +3,6 @@
 
 import "./components/accordion";
 import "./components/dialog";
-import "./components/drawer";
 import "./components/menu";
 import "./components/notification";
 import "./components/popover";
