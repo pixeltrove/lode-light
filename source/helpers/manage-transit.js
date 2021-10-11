bdf150e24dd7578070c24088349fd5bc54fa036f
Luchian <luchiancosma@gmail.com>
2021-10-11 17:00:15 +0300

    Pass all transited items using the rest parameter

--- source/helpers/manage-transit.js
+++ source/helpers/manage-transit.js
@@ -3,39 +3,29 @@
 
 const CLASS_SHOWN = "is-shown";
 
-function manageTransit(item, ...auxiliaries) {
-  const isShown = item.classList.contains(CLASS_SHOWN);
-  const phaseClass = isShown ? "is-transiting-out" : "is-transiting-in";
+function manageTransit(...items) {
+  items.forEach((item) => {
+    const isShown = item.classList.contains(CLASS_SHOWN);
+    const phaseClass = isShown ? "is-transiting-out" : "is-transiting-in";
 
-  item.classList.add(phaseClass);
-  auxiliaries.forEach((auxiliary) => {
-    auxiliary.classList.add(phaseClass);
+    item.classList.add(phaseClass);
+
+    requestAnimationFrame(() => {
+      if (isShown) {
+        item.classList.remove(CLASS_SHOWN);
+      } else {
+        item.classList.add(CLASS_SHOWN);
+      }
+    });
+
+    item.addEventListener(
+      "animationend",
+      () => {
+        item.classList.remove(phaseClass);
+      },
+      { once: true }
+    );
   });
-
-  requestAnimationFrame(() => {
-    if (isShown) {
-      item.classList.remove(CLASS_SHOWN);
-      auxiliaries.forEach((auxiliary) => {
-        auxiliary.classList.remove(CLASS_SHOWN);
-      });
-    } else if (!isShown) {
-      item.classList.add(CLASS_SHOWN);
-      auxiliaries.forEach((auxiliary) => {
-        auxiliary.classList.add(CLASS_SHOWN);
-      });
-    }
-  });
-
-  item.addEventListener(
-    "animationend",
-    () => {
-      item.classList.remove(phaseClass);
-      auxiliaries.forEach((auxiliary) => {
-        auxiliary.classList.remove(phaseClass);
-      });
-    },
-    { once: true }
-  );
 }
 
 export default manageTransit;
