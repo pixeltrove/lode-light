62622f5beb89dab5a801478d0a2364a3a25531a8
Luchian <luchiancosma@gmail.com>
2021-12-05 09:30:51 +0200

    Refactor tabset panel swap
    Filtering should not be used for simple tasks such as finding an element because it is a complex solution to a simple problem.

--- source/components/tabset.js
+++ source/components/tabset.js
@@ -6,7 +6,6 @@ import managePhasing from "../helpers/manage-phasing";
 
 const SELECTOR_TABSET = ".tabset";
 const SELECTOR_TAB = ".tabset-tab";
-const SELECTOR_PANEL = ".tabset-panel";
 const CLASS_ACTIVATED = "is-activated";
 const CLASS_PHASING_IN = "is-phasing-in";
 const CLASS_SHOWN = "is-shown";
@@ -14,21 +13,20 @@ const DATA_SHOW = "data-show";
 
 function Tabset(tabset) {
   const tabs = Array.from(tabset.querySelectorAll(SELECTOR_TAB));
-  const panels = Array.from(tabset.querySelectorAll(SELECTOR_PANEL));
   const navigationKeys = ["ArrowLeft", "ArrowRight", "Home", "End"];
 
   function swapPanel(pressedTab) {
-    const currentTab = tabs.filter((tab) => tab.classList.contains(CLASS_ACTIVATED))[0];
-    const currentPanel = panels.filter((panel) => panel.classList.contains(CLASS_SHOWN))[0];
+    const activatedTab = tabs.find((tab) => tab.classList.contains(CLASS_ACTIVATED));
     const upcomingPanelId = pressedTab.getAttribute(DATA_SHOW);
     const upcomingPanel = document.querySelector(`#${upcomingPanelId}`);
-    const isPhasing = panels.filter((panel) => panel.classList.contains(CLASS_PHASING_IN))[0];
+    const panelId = activatedTab.getAttribute(DATA_SHOW);
+    const panel = document.querySelector(`#${panelId}`);
 
-    if (!isPhasing) {
-      currentTab.classList.remove(CLASS_ACTIVATED);
-      currentTab.setAttribute("tabIndex", "-1");
-      currentPanel.classList.remove(CLASS_PHASING_IN);
-      currentPanel.classList.remove(CLASS_SHOWN);
+    if (!panel.classList.contains(CLASS_PHASING_IN)) {
+      activatedTab.classList.remove(CLASS_ACTIVATED);
+      activatedTab.setAttribute("tabIndex", "-1");
+      panel.classList.remove(CLASS_PHASING_IN);
+      panel.classList.remove(CLASS_SHOWN);
       pressedTab.classList.add(CLASS_ACTIVATED);
       pressedTab.removeAttribute("tabIndex");
       managePhasing(upcomingPanel);
