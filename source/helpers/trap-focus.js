b1b8bcd70ba0424f8b9801e900cd52dd7e83c64e
Luchian Cosma <luchiancosma@gmail.com>
2022-01-10 12:56:12 +0200

    Increase line length limit
    The maximum number of characters per line should be defined by users via editor preferences.

--- source/helpers/trap-focus.js
+++ source/helpers/trap-focus.js
@@ -2,18 +2,11 @@
 // -----------------------------------------------------------------------------
 
 function trapFocus(event, element) {
-  const focusableElements = Array.from(
-    element.querySelectorAll(
-      "a[href], audio[controls], button:not([disabled]), input:not([disabled]), select:not([disabled]), summary, textarea:not([disabled]), video[controls], [contenteditable]"
-    )
-  );
+  const focusableElements = Array.from(element.querySelectorAll("a[href], audio[controls], button:not([disabled]), input:not([disabled]), select:not([disabled]), summary, textarea:not([disabled]), video[controls], [contenteditable]"));
   const outgoingIndex = focusableElements.indexOf(document.activeElement);
   const lastIndex = focusableElements.length - 1;
 
-  if (
-    (event.shiftKey && outgoingIndex === 0) ||
-    (event.shiftKey && document.activeElement === element)
-  ) {
+  if ((event.shiftKey && outgoingIndex === 0) || (event.shiftKey && document.activeElement === element)) {
     event.preventDefault();
     focusableElements[lastIndex].focus();
   } else if (!event.shiftKey && outgoingIndex === lastIndex) {
