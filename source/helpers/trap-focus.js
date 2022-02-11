c0d6318371c29018695845948d8a593536ee8d2d
Luchian Cosma <luchiancosma@gmail.com>
2022-02-11 12:57:51 +0200

    Remove Shift key constant
    There is no benefit to storing Shift key use as a constant.

--- source/helpers/trap-focus.js
+++ source/helpers/trap-focus.js
@@ -5,12 +5,11 @@ function trapFocus(event, element) {
   const focusableElements = Array.from(element.querySelectorAll(":where(a[href], audio[controls], button:not([disabled]), input:not([disabled]), select:not([disabled]), summary, textarea:not([disabled]), video[controls], [contenteditable], [tabindex]):not([tabindex^='-'])"));
   const outgoingIndex = focusableElements.indexOf(document.activeElement);
   const lastIndex = focusableElements.length - 1;
-  const usingShift = event.shiftKey;
 
-  if (usingShift && (outgoingIndex === 0 || document.activeElement === element)) {
+  if (event.shiftKey && (outgoingIndex === 0 || document.activeElement === element)) {
     event.preventDefault();
     focusableElements[lastIndex].focus();
-  } else if (!usingShift && outgoingIndex === lastIndex) {
+  } else if (!event.shiftKey && outgoingIndex === lastIndex) {
     event.preventDefault();
     focusableElements[0].focus();
   }
