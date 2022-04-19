9bcd115028a7160f70f070d92c8463c6330348b3
Luchian Cosma <luchiancosma@gmail.com>
2022-04-19 09:48:39 +0300

    Initialize JavaScript components inside main file
    Importing components into a main file while also importing related named bindings for initialization in a separate file is a bit redundant. Initializing components inside the main file will suffice.

--- source/main.js
+++ source/main.js
@@ -1,12 +1,20 @@
+// HELPERS
+// -----------------------------------------------------------------------------
+
+import initializeComponent from "./helpers/initialize-component";
+
 // COMPONENTS
 // -----------------------------------------------------------------------------
 
-import "./components/accordion";
-import "./components/dialog";
-import "./components/menu";
-import "./components/tabset";
+import { Accordion, SELECTOR_ACCORDION } from "./components/accordion";
+import { Dialog, SELECTOR_DIALOG } from "./components/dialog";
+import { Menu, SELECTOR_MENU } from "./components/menu";
+import { Tabset, SELECTOR_TABSET } from "./components/tabset";
 
 // INITIALIZATION
 // -----------------------------------------------------------------------------
 
-import "./initialization";
+initializeComponent(Accordion, SELECTOR_ACCORDION);
+initializeComponent(Dialog, SELECTOR_DIALOG);
+initializeComponent(Menu, SELECTOR_MENU);
+initializeComponent(Tabset, SELECTOR_TABSET);
