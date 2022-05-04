faff533be2b80bf7029ec7525ee05abf87f87397
Luchian Cosma <luchiancosma@gmail.com>
2022-05-04 09:32:21 +0300

    Rename display transitions initial and final states
    Use keywords employed by CSS animations in order to provide an easier understanding of how transition helpers work.

--- source/helpers/transition-display.js
+++ source/helpers/transition-display.js
@@ -6,18 +6,18 @@ const CLASS_SHOWN = "is-shown";
 function transitionDisplay(element, transition, phase) {
   if (phase === "enter") element.classList.add(CLASS_SHOWN);
   element.classList.add(`${transition}-${phase}`);
-  element.classList.add(`${transition}-${phase}-start`);
+  element.classList.add(`${transition}-${phase}-from`);
 
   requestAnimationFrame(() => {
-    element.classList.remove(`${transition}-${phase}-start`);
-    element.classList.add(`${transition}-${phase}-end`);
+    element.classList.remove(`${transition}-${phase}-from`);
+    element.classList.add(`${transition}-${phase}-to`);
   });
 
   element.addEventListener(
     "transitionend",
     () => {
       if (phase === "leave") element.classList.remove(CLASS_SHOWN);
-      element.classList.remove(`${transition}-${phase}-end`);
+      element.classList.remove(`${transition}-${phase}-to`);
       element.classList.remove(`${transition}-${phase}`);
     },
     { once: true }
