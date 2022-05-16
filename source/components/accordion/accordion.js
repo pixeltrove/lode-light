cfaabdc728144b2128851b8544c82e4850ba7a0d
Luchian Cosma <luchiancosma@gmail.com>
2022-05-16 22:42:58 +0300

    Replace accordion top level function definition
    Component wrapper functions are unnecessary as inner module bindings cannot be accessed from the outside unless explicitly revealed via export statements. Functions required to perform various component tasks should be established directly at the module top level.

--- source/components/accordion/accordion.js
+++ source/components/accordion/accordion.js
@@ -6,14 +6,16 @@ import toggleCollapsible from "../../helpers/toggle-collapsible";
 const SELECTOR_ACCORDION = ".accordion";
 const SELECTOR_SLAT = ".accordion-slat";
 
-function Accordion(event) {
+function initializeAccordion(accordion) {
+  accordion.addEventListener("click", handleSlatClick);
+}
+
+function handleSlatClick(event) {
   const targetSlat = event.target.closest(SELECTOR_SLAT);
 
   if (targetSlat) toggleCollapsible(targetSlat);
 }
 
-document.addEventListener("click", (event) => {
-  if (event.target.closest(SELECTOR_ACCORDION)) Accordion(event);
-});
+const accordions = document.querySelectorAll(SELECTOR_ACCORDION);
 
-export default Accordion;
+accordions.forEach((accordion) => initializeAccordion(accordion));
