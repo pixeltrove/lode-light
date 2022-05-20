4ffffdae19e18f7d0ca6b829d123b58e8109c9d8
Luchian Cosma <luchiancosma@gmail.com>
2022-05-20 11:03:24 +0300

    Overhaul collapsible toggle helper
    Helpers that transition collapsibles or items with display style changes should have similar implementations as they serve similar purposes.

--- source/helpers/toggle-collapsible.js
+++ source/helpers/toggle-collapsible.js
@@ -9,34 +9,32 @@ const DATA_TOGGLE = "data-toggle";
 function toggleCollapsible(trigger) {
   const collapsibleId = trigger.getAttribute(DATA_TOGGLE);
   const collapsible = document.getElementById(collapsibleId);
-  const isPhasing = collapsible.classList.contains(CLASS_PHASING);
   const isShown = collapsible.classList.contains(CLASS_SHOWN);
 
-  if (!isPhasing) {
+  if (!isShown) collapsible.classList.add(CLASS_SHOWN);
+  collapsible.classList.add(CLASS_PHASING);
+
+  requestAnimationFrame(() => {
     trigger.classList.toggle(CLASS_ACTIVATED);
     trigger.setAttribute("aria-expanded", isShown ? "false" : "true");
-    collapsible.classList.add(CLASS_PHASING);
     collapsible.style.overflowY = "hidden";
+    collapsible.style.height = isShown ? collapsible.scrollHeight + "px" : 0;
 
     requestAnimationFrame(() => {
-      collapsible.style.height = isShown ? collapsible.scrollHeight + "px" : 0;
+      collapsible.style.height = isShown ? 0 : collapsible.scrollHeight + "px";
 
-      requestAnimationFrame(() => {
-        collapsible.style.height = isShown ? 0 : collapsible.scrollHeight + "px";
-      });
+      collapsible.addEventListener(
+        "transitionend",
+        () => {
+          collapsible.classList.remove(CLASS_PHASING);
+          if (isShown) collapsible.classList.remove(CLASS_SHOWN);
+          collapsible.style.overflowY = "";
+          collapsible.style.height = isShown ? 0 : "auto";
+        },
+        { once: true }
+      );
     });
-
-    collapsible.addEventListener(
-      "transitionend",
-      () => {
-        collapsible.classList.remove(CLASS_PHASING);
-        collapsible.classList.toggle(CLASS_SHOWN);
-        collapsible.style.overflowY = "";
-        collapsible.style.height = isShown ? 0 : "auto";
-      },
-      { once: true }
-    );
-  }
+  });
 }
 
 export default toggleCollapsible;
