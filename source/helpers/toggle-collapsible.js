0e1959a185fe188b47962cf2031a8cb890878299
Luchian Cosma <luchiancosma@gmail.com>
2022-05-20 23:52:39 +0300

    Establish collapsible transition features using ongoing phase
    Use an approach similar to that of the display transition helper to change classes or establish collapsible properties in order to simplify understanding.

--- source/helpers/toggle-collapsible.js
+++ source/helpers/toggle-collapsible.js
@@ -9,27 +9,27 @@ const DATA_TOGGLE = "data-toggle";
 function toggleCollapsible(trigger) {
   const collapsibleId = trigger.getAttribute(DATA_TOGGLE);
   const collapsible = document.getElementById(collapsibleId);
-  const isShown = collapsible.classList.contains(CLASS_SHOWN);
+  const phase = collapsible.classList.contains(CLASS_SHOWN) ? "leave" : "enter";
 
-  if (!isShown) collapsible.classList.add(CLASS_SHOWN);
+  if (phase === "enter") collapsible.classList.add(CLASS_SHOWN);
   collapsible.classList.add(CLASS_PHASING);
 
   requestAnimationFrame(() => {
     trigger.classList.toggle(CLASS_ACTIVATED);
-    trigger.setAttribute("aria-expanded", isShown ? "false" : "true");
+    trigger.setAttribute("aria-expanded", phase === "enter" ? "true" : "false");
     collapsible.style.overflowY = "hidden";
-    collapsible.style.height = isShown ? collapsible.scrollHeight + "px" : 0;
+    collapsible.style.height = phase === "enter" ? 0 : collapsible.scrollHeight + "px";
 
     requestAnimationFrame(() => {
-      collapsible.style.height = isShown ? 0 : collapsible.scrollHeight + "px";
+      collapsible.style.height = phase === "enter" ? collapsible.scrollHeight + "px" : 0;
 
       collapsible.addEventListener(
         "transitionend",
         () => {
           collapsible.classList.remove(CLASS_PHASING);
-          if (isShown) collapsible.classList.remove(CLASS_SHOWN);
+          if (phase === "leave") collapsible.classList.remove(CLASS_SHOWN);
           collapsible.style.overflowY = "";
-          collapsible.style.height = isShown ? 0 : "auto";
+          collapsible.style.height = phase === "enter" ? "auto" : 0;
         },
         { once: true }
       );
