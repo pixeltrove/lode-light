35a038a307d44ed805bb796d45549b73baafd297
Luchian Cosma <luchiancosma@gmail.com>
2022-05-23 10:21:57 +0300

    Rename accordion slat toggle helper and animation

---
+++ source/helpers/toggle-expandable.js
@@ -0,0 +1,40 @@
+// TOGGLE EXPANDABLE
+// -----------------------------------------------------------------------------
+
+const CLASS_ACTIVATED = "activated";
+const CLASS_SHOWN = "shown";
+const DATA_TOGGLE = "data-toggle";
+
+function toggleExpandable(trigger) {
+  const expandableId = trigger.getAttribute(DATA_TOGGLE);
+  const expandable = document.getElementById(expandableId);
+  const phase = expandable.classList.contains(CLASS_SHOWN) ? "leave" : "enter";
+  const phasingClass = `expand-regular-${phase}`;
+
+  if (phase === "enter") expandable.classList.add(CLASS_SHOWN);
+  expandable.classList.add(phasingClass);
+
+  requestAnimationFrame(() => {
+    trigger.classList.toggle(CLASS_ACTIVATED);
+    trigger.setAttribute("aria-expanded", phase === "enter" ? "true" : "false");
+    expandable.style.overflowY = "hidden";
+    expandable.style.height = phase === "enter" ? 0 : expandable.scrollHeight + "px";
+
+    requestAnimationFrame(() => {
+      expandable.style.height = phase === "enter" ? expandable.scrollHeight + "px" : 0;
+
+      expandable.addEventListener(
+        "transitionend",
+        () => {
+          expandable.classList.remove(phasingClass);
+          if (phase === "leave") expandable.classList.remove(CLASS_SHOWN);
+          expandable.style.overflowY = "";
+          expandable.style.height = phase === "enter" ? "auto" : 0;
+        },
+        { once: true }
+      );
+    });
+  });
+}
+
+export default toggleExpandable;
