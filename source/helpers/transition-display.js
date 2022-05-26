b74d075b92191f0daa87253773a49568bff26d61
Luchian Cosma <luchiancosma@gmail.com>
2022-05-26 10:14:10 +0300

    Clean up helper definition

--- source/helpers/transition-display.js
+++ source/helpers/transition-display.js
@@ -5,9 +5,9 @@ const CLASS_SHOWN = "shown";
 
 function transitionDisplay(element, transition) {
   const phase = element.classList.contains(CLASS_SHOWN) ? "leave" : "enter";
+  const transitionClass = `${transition}-${phase}`;
   const isEntering = phase === "enter" ? true : false;
   const isExpandable = transition === "expand-regular" ? true : false;
-  const transitionClass = `${transition}-${phase}`;
 
   if (isEntering) element.classList.add(CLASS_SHOWN);
   element.classList.add(transitionClass);
@@ -21,20 +21,21 @@ function transitionDisplay(element, transition) {
       element.style.overflowY = "hidden";
       element.style.height = isEntering ? 0 : element.scrollHeight + "px";
     }
+
     requestAnimationFrame(() => {
       if (isExpandable) element.style.height = isEntering ? element.scrollHeight + "px" : 0;
 
       element.addEventListener(
         "transitionend",
         () => {
+          if (!isEntering) element.classList.remove(CLASS_SHOWN);
+          element.classList.remove(transitionClass);
           if (!isExpandable) {
             element.classList.remove(`${transitionClass}-to`);
           } else {
             element.style.overflowY = "";
             element.style.height = isEntering ? "auto" : 0;
           }
-          element.classList.remove(transitionClass);
-          if (!isEntering) element.classList.remove(CLASS_SHOWN);
         },
         { once: true }
       );
