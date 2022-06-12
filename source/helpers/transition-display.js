3dd5f4a7e506fdc66ea311fc626101c78dc65999
Luchian Cosma <luchiancosma@gmail.com>
2022-06-12 06:38:34 +0300

    Trigger transition phases using booleans only
    Conditional statements that employ booleans can be used to decide which transition phases should be triggered, although it does seem overly convoluted.

--- source/helpers/transition-display.js
+++ source/helpers/transition-display.js
@@ -4,87 +4,86 @@
 const CLASS_SHOWN = "shown";
 
 function transitionDisplay(element, effect, timing = "regular") {
-  if (effect != "wait") {
-    const isShowing = element.classList.contains(CLASS_SHOWN);
-    const enterTransition = `${effect}-${timing}-enter`;
-    const enterFrom = `${enterTransition}-from`;
-    const enterTo = `${enterTransition}-to`;
-    const leaveTransition = `${effect}-${timing}-leave`;
-    const leaveFrom = `${leaveTransition}-from`;
-    const leaveTo = `${leaveTransition}-to`;
+  const enterTransition = `${effect}-${timing}-enter`;
+  const enterFrom = `${enterTransition}-from`;
+  const enterTo = `${enterTransition}-to`;
+  const leaveTransition = `${effect}-${timing}-leave`;
+  const leaveFrom = `${leaveTransition}-from`;
+  const leaveTo = `${leaveTransition}-to`;
+  const isWaiting = effect === "wait" ? true : false;
+  const isShowing = element.classList.contains(CLASS_SHOWN);
+  const isEntering = element.classList.contains(enterTransition);
 
-    function enter() {
-      element.classList.add(CLASS_SHOWN);
-      element.classList.add(enterTransition);
-      element.classList.add(enterFrom);
+  function enter() {
+    element.classList.add(CLASS_SHOWN);
+    element.classList.add(enterTransition);
+    element.classList.add(enterFrom);
 
-      requestAnimationFrame(() => {
-        element.classList.remove(enterFrom);
-        element.classList.add(enterTo);
-
-        element.addEventListener("transitionend", enterEnd);
-      });
-    }
-
-    function enterEnd() {
-      element.classList.remove(enterTo);
-      element.classList.remove(enterTransition);
-
-      element.removeEventListener("transitionend", enterEnd);
-    }
-
-    function cancelEnter() {
-      element.classList.remove(enterTo);
-      element.classList.add(enterFrom);
-
-      element.addEventListener("transitionend", cancelEnterEnd);
-    }
-
-    function cancelEnterEnd() {
+    requestAnimationFrame(() => {
       element.classList.remove(enterFrom);
-      element.classList.remove(enterTransition);
-      requestAnimationFrame(() => {
-        element.classList.remove(CLASS_SHOWN);
-      });
+      element.classList.add(enterTo);
 
-      element.removeEventListener("transitionend", cancelEnterEnd);
-    }
+      element.addEventListener("transitionend", enterEnd);
+    });
+  }
 
-    function leave() {
-      element.classList.add(leaveTransition);
-      element.classList.add(leaveFrom);
+  function enterEnd() {
+    element.classList.remove(enterTo);
+    element.classList.remove(enterTransition);
 
-      requestAnimationFrame(() => {
-        element.classList.remove(leaveFrom);
-        element.classList.add(leaveTo);
+    element.removeEventListener("transitionend", enterEnd);
+  }
 
-        element.addEventListener("transitionend", leaveEnd);
-      });
-    }
+  function cancelEnter() {
+    element.classList.remove(enterTo);
+    element.classList.add(enterFrom);
 
-    function leaveEnd() {
-      element.classList.remove(leaveTo);
-      element.classList.remove(leaveTransition);
-      requestAnimationFrame(() => {
-        element.classList.remove(CLASS_SHOWN);
-      });
+    element.addEventListener("transitionend", cancelEnterEnd);
+  }
 
-      element.removeEventListener("transitionend", leaveEnd);
-    }
+  function cancelEnterEnd() {
+    element.classList.remove(enterFrom);
+    element.classList.remove(enterTransition);
+    requestAnimationFrame(() => {
+      element.classList.remove(CLASS_SHOWN);
+    });
 
-    if (!isShowing) enter();
-    if (isShowing && element.classList.contains(enterTransition)) {
-      cancelEnter();
-    }
-    if (isShowing && !element.classList.contains(enterTransition)) leave();
-  } else {
-    if (!element.classList.contains(CLASS_SHOWN)) {
-      element.classList.add(CLASS_SHOWN);
-    } else {
-      setTimeout(() => {
-        element.classList.remove(CLASS_SHOWN);
-      }, "280");
-    }
+    element.removeEventListener("transitionend", cancelEnterEnd);
+  }
+
+  function leave() {
+    element.classList.add(leaveTransition);
+    element.classList.add(leaveFrom);
+
+    requestAnimationFrame(() => {
+      element.classList.remove(leaveFrom);
+      element.classList.add(leaveTo);
+
+      element.addEventListener("transitionend", leaveEnd);
+    });
+  }
+
+  function leaveEnd() {
+    element.classList.remove(leaveTo);
+    element.classList.remove(leaveTransition);
+    requestAnimationFrame(() => {
+      element.classList.remove(CLASS_SHOWN);
+    });
+
+    element.removeEventListener("transitionend", leaveEnd);
+  }
+
+  if (!isShowing && !isWaiting) enter();
+  if (isShowing && isEntering && !isWaiting) {
+    cancelEnter();
+  }
+  if (isShowing && !isEntering && !isWaiting) leave();
+
+  if (!isShowing && isWaiting) element.classList.add(CLASS_SHOWN);
+  if (isShowing && isWaiting) {
+    setTimeout(() => {
+      element.classList.remove(CLASS_SHOWN);
+    }, "280");
   }
 }
 
